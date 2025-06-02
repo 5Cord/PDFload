5d14aaaa469194ca49ad952cb749441d2f404ed3
Vaytovich Dmitriy Valentinovich <102501329+5Cord@users.noreply.github.com>
2025-06-02 17:53:05 +0300

    feat: update home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
+import { Text } from '@consta/uikit/TextDeprecated';
 
 export default function HomePage() {
     return (
-        <div>HomePage</div>
+        <Theme preset={presetGpnDefault}>
+            <Text
+                size="3xl"
+                as="h1"
+                align="center"
+                view="primary"
+                weight="bold"
+            >
+                Главная страница
+            </Text>
+        </Theme>
     )
 }
